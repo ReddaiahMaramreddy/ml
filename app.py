1e01a7ffa1630d922fc196475e7a42a14900d9e6
ReddaiahMaramreddy <81581634+ReddaiahMaramreddy@users.noreply.github.com>
2021-07-26 13:08:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def predict():
 
 # Allow the Flask app to launch from the command line
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000)
+    port = int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0', port= port)
